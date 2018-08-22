bdab6829fda3ba1250ee9157fd16958192342b29
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-08-22 15:35:34 -0400

    X-Axis displays. Bars not yet lining up correctly.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//D3 resources: https://www.youtube.com/watch?v=znBBk_M95QY
+//https://www.youtube.com/watch?v=11QMX7vA22Y&t=785s
+//timeParse resources: https://github.com/d3/d3-time#_interval
+//https://stackoverflow.com/questions/32428122/what-scale-to-use-for-representing-years-only-on-x-axis-in-d3-js
+
 document.addEventListener('DOMContentLoaded',function(){
   req=new XMLHttpRequest();
   req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',true);
@@ -6,68 +11,62 @@ document.addEventListener('DOMContentLoaded',function(){
     json=JSON.parse(req.responseText);
     var data = json.data
     
-    //Years ('57 - '15)
-    var anos = []
-    json.data.forEach(function(val){
-      anos.push(val[0])
+    //Modify data set so it shows the parsed time.
+    var parseDate = d3.timeParse("%Y-%m-%d")
+    var data2 = []
+    data.forEach(function(val){
+      data2.push([parseDate(val[0]), val[1]])
     })
-    
-    //GDP
-    var gdp = []
-    json.data.forEach(function(val){
-      gdp.push(val[1])
-    })
-  
+
     const w = 1000; //width
     const h = 600; //height
     const padding = 100 //padding
     
     //Scale for x-axis (years)
-    const xScale = d3.scaleLinear()
-                     .domain([d3.min(anos), d3.max(anos)]) //removed callback because anos array is one-dimensional
-                     .range([0, (w - (padding))]);
-                   // .range([0, w]);  //tried without padding, but numbers didn't show
-                   // .range([padding, w-padding]); //FCC method (for scatterplots)
-    
+    const xScale = d3.scaleTime()
+                     .domain([d3.min(data2, d => d[0]), d3.max(data2, d => d[0])]) 
+                     .range([0, (w - (2*padding))]);
+
     //Scale for y-axis (GDP)
     const yScale = d3.scaleLinear()
-                     .domain([0, d3.max(gdp)]) //removed callback because gdp array is one-dimensional
+                     .domain([0, d3.max(data2, d => d[1])])
                      .range([(h - (2*padding)), 0])
-                    //.range([h, 0])  //tried without padding, but numbers didn't show
-                    //.range([h-padding, padding]) //FCC method (for scatterplots)
-    
+
     //Set the X and Y axes
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale)
+                   //  .ticks(d3.timeYear.every(0))
+    const yAxis = d3.axisLeft(yScale)
     
-    
-    const svg=d3.select("ul")
+    // Adds the svg canvas
+    const svg=d3.select("a")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h)
                 
-    
+    //Add the rectangles (bars)
     svg.selectAll("rect")
-                .data(gdp)
+                .data(data2)
                 .enter()
                 .append("rect")
                 .attr("width", 3)
-                .attr("height", (d, i) => {return d/50})
+                .attr("height", (d, i) => {return d[1]/50})
                 .attr("x", (d, i) => {return (i * 3) + padding})
-                .attr("y", (d, i) => {return h - 2*padding+5 - d/50}) //Moved down 5px to accomodate y-axis. See below.
-                .attr("class", "graph")
+                .attr("y", (d, i) => {return h - 2*padding+5 - d[1]/50})
+                .attr("class", "bar")
                 .append("title")
-                .text(d => "$" + d + " Billion")
+                .text(d => d[0] + " $" + d[1] + " Billion")
 
-             //move the X-axis
+            //Add and move the X-axis
              svg.append("g")
-                .attr("transform", "translate(" + (padding) + ", " + (h-(2*padding)+5) + ")") //Moved down 5px to accomodate y-axis. See below
-                .call(xAxis);
+                .attr("transform", "translate(" + (padding) + ", " + (h-(2*padding)+5) + ")")
+                .call(xAxis)
+                .attr("id", "x-axis");
     
-             //move the Y-axis
+             //Add and move the Y-axis
              svg.append("g")
-                .attr("transform", "translate(" + (padding) + ", " + (5) + ")") //Moved down 5px, so top number would not be cut off
-                .call(yAxis);
+                .attr("transform", "translate(" + (padding) + ", " + (5) + ")")
+                .call(yAxis)
+                .attr("id", "y-axis");
 
   };
 
